5ee11c6d62f1f4a9052da8dbb07b8b52109f45a9
FrankCCCCC <43928493+FrankCCCCC@users.noreply.github.com>
2021-07-20 19:25:11 +0800

    Update MathJax.js

--- MathJax.js
+++ MathJax.js
@@ -1,9 +1,20 @@
-<script type="text/x-mathjax-config">
-  MathJax.Hub.Config({
-    jax: ["input/TeX","input/MathML","input/AsciiMath","output/CommonHTML"],
-    extensions: ["tex2jax.js","mml2jax.js","asciimath2jax.js","MathMenu.js","MathZoom.js","AssistiveMML.js", "a11y/accessibility-menu.js"],
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['$', '$'] ],
+    displayMath: [['$$', '$$'] ],
+    processEscapes: true,
+    processEnvironments: true,
+    skipTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
     TeX: {
-      extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"]
-    }
-  });
-</script>
+      equationNumbers: {autoNumber: 'AMS'},
+      extensions: ['AMSmath.js', 'AMSsymbols.js', 'color.js'],
+    },
+  }
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML'
+  script.async = true;
+  document.head.appendChild(script);
+})();
